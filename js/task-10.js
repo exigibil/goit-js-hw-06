ed20b22ae8a416bde6dfda80845a1174fa39d5d3
exigibil <adrian.topa04@gmail.com>
2023-12-27 22:09:11 +0200

    finish homework
    finish homework

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,45 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const divElem = document.querySelector("div#boxes");
+const inputVal = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+
+
+function createBox(amount) {
+divElem.innerHTML = "";
+
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+  const box = document.createElement("div");
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+  box.style.backgroundColor = getRandomHexColor();
+  divElem.appendChild(box);
+
+  width +=10;
+  height +=10;
+
+ }
+}
+
+function destroyBox() {
+  divElem.innerHTML = "";
+  inputVal.value= "";
+}
+
+createBtn.addEventListener("click", e => {
+  const amount = parseInt(inputVal.value, 10);
+  if (amount) {
+    createBox(amount);
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBox);
+ 
+
